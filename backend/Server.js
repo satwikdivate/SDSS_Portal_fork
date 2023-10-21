3f4a9665ce532bad34d10a48927691612a370cb2
JoyCode4 <joywadhonkar4@gmail.com>
2023-10-21 13:38:36 +0530

    1.database config 2.nodemon start cmd 3.updation on the server

--- backend/Server.js
+++ backend/Server.js
@@ -1,44 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Student = require('.//db/studentModel'); // Import the student schema/model
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const db = require("./config/db");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/studentbio', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-
-app.get('/api/students', async (req, res) => {
+app.get("/api/students", async (req, res) => {
   try {
     const students = await Student.find();
     res.json(students);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 });
 
-app.post('/api/students', async (req, res) => {
+app.post("/api/students", async (req, res) => {
   try {
     const newStudent = new Student(req.body);
     const savedStudent = await newStudent.save();
     res.json(savedStudent);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 });
 
---
+++ backend/db/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    id: {
+      type: String,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
